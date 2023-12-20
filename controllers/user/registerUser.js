8fc947e1f2e34565f5b1e4be796902a3f54af1d6
Inam Ul Rehman <inam@dryermaster.com>
2023-12-20 09:48:33 -0500

    update

--- controllers/user/registerUser.js
+++ controllers/user/registerUser.js
@@ -5,9 +5,19 @@ const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const jose = require('jose');
 
+const nextBillingDate = () => {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+
+  // Set the billing period end date to July 1st of the next year
+  const nextBillingDate = new Date(currentYear + 1, 6, 1); // Months are 0-indexed, so 6 is July
+
+  return nextBillingDate;
+};
 // ==========>>>>>> Create operation - create a user
 const registerUser = async (req, res, next) => {
   try {
+    const subscriptionExpiry = nextBillingDate();
     const { firstName, lastName, email, password, dmSerial } = req.body;
     const isFirstAccount = await User.countDocuments({});
     const role = isFirstAccount === 0 ? 'admin' : 'user';
@@ -18,11 +28,10 @@ const registerUser = async (req, res, next) => {
       password,
       role,
       dmSerial,
-      subscriptionExpiry: new Date(
-        new Date().setFullYear(new Date().getFullYear() + 1)
-      ),
+      subscriptionExpiry,
     });
     const token = await user.createJWT();
+
     res.status(StatusCodes.CREATED).json({
       success: true,
       message: 'User created successfully!',
