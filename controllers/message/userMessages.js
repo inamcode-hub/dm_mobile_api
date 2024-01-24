5729e345c49a8a2031d16936f5e013f35f593d91
Inam Ul Rehman <inam@dryermaster.com>
2024-01-24 11:57:17 -0500

    update

--- controllers/message/userMessages.js
+++ controllers/message/userMessages.js
@@ -4,12 +4,26 @@ const Message = require('../../models/Message');
 // Create operation
 const userMessages = async (req, res, next) => {
   try {
-    const messages = await Message.find() // remove createdBy and _id
-      .select('-createdBy -_id');
+    const page = parseInt(req.query.page) || 1; // Get the page number from the query parameters
+    const messagesPerPage = parseInt(req.query.limit) || 10;
+
+    // Count total messages
+    const totalMessages = await Message.countDocuments();
+
+    const messages = await Message.find()
+      .select('-createdBy -_id')
+      .sort({ createdAt: -1 }) // Sort messages by createdAt in descending order (most recent first)
+      .skip((page - 1) * messagesPerPage) // Skip the previous pages
+      .limit(messagesPerPage); // Limit the number of messages per page
+
+    const totalPages = Math.ceil(totalMessages / messagesPerPage);
 
     res.status(StatusCodes.OK).json({
       success: true,
       message: 'Messages retrieved successfully',
+      totalMessages,
+      totalPages,
+      messagesPerPage,
       result: messages,
     });
   } catch (err) {
