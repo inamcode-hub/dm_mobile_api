bd8eb3b059b81f2f1b19f16d4d50fb90ab133adb
Inam Ul Rehman <inam@dryermaster.com>
2024-01-26 08:35:21 -0500

    update

--- controllers/message/userMessages.js
+++ controllers/message/userMessages.js
@@ -1,6 +1,5 @@
 const { StatusCodes } = require('http-status-codes');
 const Message = require('../../models/Message');
-const Employee = require('../../models/Employee');
 
 const userMessages = async (req, res, next) => {
   try {
@@ -11,30 +10,18 @@ const userMessages = async (req, res, next) => {
     const totalMessages = await Message.countDocuments();
 
     let messages = await Message.find()
-      .select('-content -__v -updatedAt')
+      .select('-content -__v -updatedAt -createdBy')
       .sort({ createdAt: -1 })
       .skip((page - 1) * messagesPerPage)
       .limit(messagesPerPage);
 
-    // Function to return the employee name
-    const getEmployeeName = async (employeeId) => {
-      const employee = await Employee.findById(employeeId);
-      return employee.firstName + ' ' + employee.lastName;
-    };
+    // Add readMessage property to each message
+    messages = messages.map((message) => ({
+      ...message.toObject(), // Convert document to plain JavaScript object
+      readMessage: message.readBy.includes(userId),
+      readBy: undefined, // Remove readBy property from the response
+    }));
 
-    // Fetch and add sender name to each message
-    const result = await Promise.all(
-      messages.map(async (message) => {
-        const author = await getEmployeeName(message.createdBy);
-        return {
-          ...message.toObject(), // Convert document to plain JavaScript object
-          readMessage: message.readBy.includes(userId),
-          author, // Add resolved sender name
-          readBy: undefined, // Remove readBy property from the response
-          createdBy: undefined, // Remove createdBy property from the response
-        };
-      })
-    );
     //readBy property removed from the response
 
     const totalPages = Math.ceil(totalMessages / messagesPerPage);
@@ -45,7 +32,7 @@ const userMessages = async (req, res, next) => {
       totalMessages,
       totalPages,
       messagesPerPage,
-      result,
+      result: messages,
     });
   } catch (err) {
     next(err);
