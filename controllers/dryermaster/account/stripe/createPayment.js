a568c21267a08526d40c8d30256c276235cb36b4
Inam Ul Rehman <inam@dryermaster.com>
2024-02-12 09:55:35 -0500

    update

--- controllers/dryermaster/account/stripe/createPayment.js
+++ controllers/dryermaster/account/stripe/createPayment.js
@@ -7,42 +7,72 @@ const User = require('../../../../models/User');
 const createPayment = async (req, res, next) => {
   const { paymentMethodId } = req.body; // paymentMethodId is the id of the Stripe Payment Method
   const _id = req.user.userId;
-  console.log('paymentMethodId', paymentMethodId);
+
   try {
     let user = await User.findOne({ _id });
 
+    let customerId; // Declare variable to hold the Stripe customer ID
+
     if (!user.stripeCustomerId) {
+      // If user does not have a Stripe customer ID, create a new Stripe customer
       const customer = await stripe.customers.create({
         payment_method: paymentMethodId,
         email: user.email,
+        name: user.firstName + ' ' + user.lastName,
         invoice_settings: {
           default_payment_method: paymentMethodId,
         },
       });
 
-      user.stripeCustomerId = customer.id;
-      await user.save();
+      // Update the user in the database with the new Stripe customer ID
+      const updatedUser = await User.findByIdAndUpdate(
+        _id,
+        { stripeCustomerId: customer.id },
+        { new: true }
+      ); // Use { new: true } to return the updated user object
+      customerId = updatedUser.stripeCustomerId; // Use the updated Stripe customer ID for the payment intent
+    } else {
+      // If user already has a Stripe customer ID, use it for the payment intent
+      customerId = user.stripeCustomerId;
     }
 
-    // Create a Payment Intent instead of a direct charge
+    // Now, create a Payment Intent with the correct Stripe customer ID
     const paymentIntent = await stripe.paymentIntents.create({
+      payment_method_types: ['card'],
       amount: 10000, // amount in cents
       currency: 'usd',
-      customer: user.stripeCustomerId,
+      customer: customerId,
       payment_method: paymentMethodId,
       off_session: true, // Set to true if the customer is not present during payment
       confirm: true, // Automatically confirm the payment intent
-      description: 'My First Test Charge (created for API docs)',
+      metadata: {
+        dryerMasterUserId: _id,
+      },
+      description: 'Dryer Master Payment',
+      statement_descriptor: 'Dryer Master Payment',
+      receipt_email: user.email,
     });
 
-    console.log('user', user);
     res.status(StatusCodes.CREATED).json({
       success: true,
-      message: 'Payment created successfully',
+      message: 'Payment successful. Your Subscription has been activated.',
       result: paymentIntent,
     });
   } catch (err) {
-    next(err);
+    console.error(err); // Log for server-side debugging
+    if (err.type === 'StripeCardError') {
+      // Handle common card errors
+      res.status(StatusCodes.BAD_REQUEST).json({
+        success: false,
+        message: err.message, // e.g., "Your card has expired."
+      });
+    } else {
+      // Handle other errors
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        success: false,
+        message: 'An error occurred processing your payment. Please try again.',
+      });
+    }
   }
 };
 
